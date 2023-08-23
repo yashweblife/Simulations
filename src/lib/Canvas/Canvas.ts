40dd2c49bb975fcaba27475ab0652e69ccaffdfd
yashweblife <yashweblife20@gmail.com>
2023-08-23 16:56:53 -0500

    add clear

--- src/lib/Canvas/Canvas.ts
+++ src/lib/Canvas/Canvas.ts
@@ -1,9 +1,12 @@
 export class Canvas{
     public dom:HTMLCanvasElement=document.createElement("canvas");
     public ctx:CanvasRenderingContext2D=this.dom.getContext("2d")!;
-    constructor(parent:HTMLElement){
+    constructor(parent:HTMLElement=document.body){
         parent.appendChild(this.dom);
         this.dom.width=300;
         this.dom.height=300;
+    }
+    public clear(){
+        this.ctx.clearRect(0,0,this.dom.width,this.dom.height);
     }
 }
