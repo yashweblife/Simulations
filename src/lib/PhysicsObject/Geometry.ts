1fb7f835e678db3749d561b3a8c8e4226af91b34
yashweblife <yashweblife20@gmail.com>
2023-08-26 00:30:56 -0500

    add box/circle fix

--- src/lib/PhysicsObject/Geometry.ts
+++ src/lib/PhysicsObject/Geometry.ts
@@ -2,8 +2,8 @@ import { Vector } from "../Vector/Vector";
 
 export class Geometry{
     constructor(public vertices:Vector[]=[]){}
-    public getOffsetVertices(position:Vector):Vector[]{
-        return this.vertices.map((vertex)=>vertex.getAdd(position));
+    public getOffsetVertices(position:Vector):Geometry{
+        return new Geometry(this.vertices.map((vertex)=>vertex.getAdd(position)));
     }
     public getScaledXVertices(scale:number):Geometry{
         return new Geometry(this.vertices.map((vertex)=>vertex.getScaleX(scale)));
@@ -15,3 +15,20 @@ export class Geometry{
         return new Geometry(this.vertices.map((vertex)=>vertex.getRotated(angle)));
     }    
 }
+
+
+const circleShape = [];
+const boxShape = [
+    new Vector(-1, -1),
+    new Vector(1, -1),
+    new Vector(1, 1),
+    new Vector(-1, 1),
+    new Vector(-1, -1)
+];
+for(let i=-Math.PI;i<=Math.PI;i+=0.1){
+    circleShape.push(new Vector(Math.cos(i), Math.sin(i)));
+}
+circleShape.push(new Vector(Math.cos(-Math.PI), Math.sin(-Math.PI)));
+
+export const CIRCLE = new Geometry(circleShape); 
+export const BOX = new Geometry(boxShape);
