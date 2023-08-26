ef254f396d706271687d3a80992e73ebdb835cbe
yashweblife <yashweblife20@gmail.com>
2023-08-26 02:17:58 -0500

    finally a working grid (kind of)

--- src/lib/Canvas/Canvas.ts
+++ src/lib/Canvas/Canvas.ts
@@ -1,4 +1,4 @@
-import { Vector } from "../Vector/Vector";
+import { Vector, toRad } from "../Vector/Vector";
 
 export class Canvas{
     public dom:HTMLCanvasElement=document.createElement("canvas");
@@ -9,42 +9,35 @@ export class Canvas{
         this.dom.height=300;
     }
     public grid(){
-        const rotation_x = new Vector(0 * (180/Math.PI),0 * (180/Math.PI),45 * (180/Math.PI));
-        const scale = 20;
-        const x_lines = [];
-        const y_lines = [];
-        for(let i=0;i<this.dom.height;i+=scale){
-            x_lines.push({
-                start:new Vector(0,i,0),
-                end:new Vector(this.dom.width,i,0)
-            })
-
-            y_lines.push({
-                start:new Vector(i,0,0),
-                end:new Vector(i,this.dom.height,0)
-            })
-            
+        //create a zoomable rotatable grid 
+        const zoom =20;
+        const rotation = new Vector(toRad(80),toRad(10),toRad());
+        const pos = new Vector(0,150,0);
+        const size = new Vector(this.dom.width,this.dom.height);
+        for(let i=0;i<size.components[0]/zoom;i++){
+            this.ctx.strokeStyle = 'rgba(0,0,0,0.2)'
+            {
+                const start = new Vector(i * zoom, 0,0);
+                const end = new Vector(i * zoom, size.components[1],0);
+                start.rotate(rotation).add(pos);
+                end.rotate(rotation).add(pos);
+                this.line(start, end);
+            }{
+                const start = new Vector(0, i * zoom,0);
+                const end = new Vector(size.components[0], i * zoom,0);
+                start.rotate(rotation).add(pos);
+                end.rotate(rotation).add(pos);
+                this.line(start, end);
+            }
         }
-        for(let i=0;i<x_lines.length;i++){
-            this.ctx.beginPath();
-            const x_start = x_lines[i].start.scale(scale).getRotated(rotation_x);
-            const x_end = x_lines[i].end.scale(scale).getRotated(rotation_x);
-            this.ctx.moveTo(x_start.components[0],x_end.components[1]);
-            this.ctx.lineTo(x_lines[i].end.components[0],x_lines[i].end.components[1]);
-            this.ctx.strokeStyle = "rgba(0,0,0,0.2)";
-            this.ctx.stroke();
-            this.ctx.closePath();
-        }
-        for(let i=0;i<y_lines.length;i++){
-            this.ctx.beginPath();
-            const y_start = y_lines[i].start.scale(scale).getRotated(rotation_x);
-            const y_end = y_lines[i].end.scale(scale).getRotated(rotation_x);
-            this.ctx.moveTo(y_start.components[0],y_start.components[1]);
-            this.ctx.lineTo(y_end.components[0],y_end.components[1]);
-            this.ctx.strokeStyle = "rgba(0,0,0,0.2)";
-            this.ctx.stroke();
-            this.ctx.closePath();
-        }
+        
+    }
+    public line(start:Vector, end:Vector, color:string="rgba(0,0,0,0.2)"){
+        this.ctx.beginPath();
+        this.ctx.moveTo(start.components[0], start.components[1]);
+        this.ctx.lineTo(end.components[0], end.components[1]);
+        this.ctx.stroke();
+        this.ctx.closePath();
     }
     public clear(){
         this.ctx.clearRect(0,0,this.dom.width,this.dom.height);
