a9e5fd863fbdd6f73d23a558b337389b85db818f
yashweblife <yashweblife20@gmail.com>
2023-08-26 02:24:06 -0500

    more functions

--- src/lib/Vector/Vector.ts
+++ src/lib/Vector/Vector.ts
@@ -19,9 +19,55 @@ export class Vector{
         }
         return this;
     }
-    scale(scalar:number){
-        const components=this.components.map((component)=>component*scalar);
-        return this
+    div(scalar:number){
+        for(let i=0;i<this.components.length;i++){
+            this.components[i]/=scalar;
+        }
+        return this;
+    }
+    magnitude(){
+        let sum=0;
+        for(let i=0;i<this.components.length;i++){
+            sum+=this.components[i]**2;
+        }
+        return Math.sqrt(sum);
+    }
+    normalize(scale:number=1){
+        const length = this.magnitude();
+        for(let i=0;i<this.components.length;i++){
+            this.components[i]*=scale/length;
+        }
+        return this;
+    }
+    rotateX(angle:number){
+        const y = this.components[1];
+        const z = this.components[2] || 0;
+        const ax = angle;
+        this.components[1] = y*Math.cos(ax) - z*Math.sin(ax);
+        this.components[2] = y*Math.sin(ax) + z*Math.cos(ax);
+        return(this);
+    }
+    rotateY(angle:number){
+        const x = this.components[0];
+        const z = this.components[2] || 0;
+        const ay = angle;
+        this.components[0] = x*Math.cos(ay) + z*Math.sin(ay);
+        this.components[2] = -x*Math.sin(ay) + z*Math.cos(ay);
+        return(this);
+    }
+    rotateZ(angle:number){
+        const x = this.components[0];
+        const y = this.components[1];
+        const az = angle;
+        this.components[0] = x*Math.cos(az) - y*Math.sin(az);
+        this.components[1] = x*Math.sin(az) + y*Math.cos(az);
+        return(this);
+    }
+    rotate(angle:Vector){
+        this.rotateX(angle.components[0]);
+        this.rotateY(angle.components[1]);
+        this.rotateZ(angle.components[2]);
+        return(this);
     }
     getScaleX(scalar:number){
         return new Vector(this.components[0]*scalar, this.components[1]);
@@ -60,4 +106,8 @@ export class Vector{
         const z_prime = -x*sin_y + y*cos_y*sin_x + z*cos_x*cos_y;
         return new Vector(x_prime, y_prime, z_prime);
     } 
+}
+
+export function toRad(deg:number=0){
+    return deg*(Math.PI/180);
 }
