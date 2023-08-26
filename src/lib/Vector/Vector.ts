288f9d608dd7807ab6ce2f3ad238f5d561976cba
yashweblife <yashweblife20@gmail.com>
2023-08-26 00:29:06 -0500

    fix rotation

--- src/lib/Vector/Vector.ts
+++ src/lib/Vector/Vector.ts
@@ -29,11 +29,35 @@ export class Vector{
     getScaleY(scalar:number){
         return new Vector(this.components[0], this.components[1]*scalar);
     }
+    getRotatedX(angle:number){
+        const x=this.components[0];
+        const y=this.components[1];
+        const z=this.components[2] || 0;
+        const cos=Math.cos(angle);
+        const sin=Math.sin(angle);
+        return new Vector(x, y*cos-z*sin, y*sin+z*cos);
+    }
+    getRotatedY(angle:number){
+        const x=this.components[0];
+        const y=this.components[1];
+        const z=this.components[2] || 0;
+        const cos=Math.cos(angle);
+        const sin=Math.sin(angle);
+        return new Vector(x*cos+z*sin, y, -x*sin+z*cos);
+    }
     getRotated(angle:Vector){
         const x=this.components[0];
         const y=this.components[1];
-        const cos=Math.cos(angle.components[0]);
-        const sin=Math.sin(angle.components[1]);
-        return new Vector(x*cos-y*sin, x*sin+y*cos);
+        const z=this.components[2] || 0;        
+        const cos_x=Math.cos(angle.components[0]);
+        const sin_x=Math.sin(angle.components[0]);
+        const cos_y=Math.cos(angle.components[1]);
+        const sin_y=Math.sin(angle.components[1]);
+        const cos_z=Math.cos(angle.components[2]);
+        const sin_z=Math.sin(angle.components[2]);
+        const x_prime = x*cos_y*cos_z + y*(cos_z*sin_x*sin_y-cos_x*sin_z) + z*(cos_x*cos_z*sin_y+sin_x*sin_z);
+        const y_prime = x*cos_y*sin_z + y*(cos_x*cos_z+sin_x*sin_y*sin_z) + z*(-cos_z*sin_x+cos_x*sin_y*sin_z);
+        const z_prime = -x*sin_y + y*cos_y*sin_x + z*cos_x*cos_y;
+        return new Vector(x_prime, y_prime, z_prime);
     } 
 }
