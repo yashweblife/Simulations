dbd42c8d6935bfb7dc3553751697eda9ec24821c
yashweblife <yashweblife20@gmail.com>
2023-08-26 00:28:57 -0500

    add grid

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,28 @@
-import { Canvas, Vector } from "./lib";
+import { Canvas } from "./lib";
 import { PhysicsObject } from "./lib/PhysicsObject/PhysicsObject";
 
 const canvas = new Canvas();
 
-const body = new PhysicsObject();
-body.position = new Vector(100, 100);
-const shape = []
-for (let i = 0; i < Math.PI*2; i+=0.1) {
-    shape.push(new Vector(Math.cos(i)*10, Math.sin(i)*10));
-}
-body.setShape(shape);
-const body2 = new PhysicsObject();
-body2.position = new Vector(200, 200);
-const shape2 = [
-new Vector(-1, -1),
-new Vector(1, -1),
-new Vector(1, 1),
-new Vector(-1, 1),
-new Vector(-1, -1)]
+const box = PhysicsObject.BOX();
+box.position.components[0] = 150;
+box.position.components[1] = 150;
+box.scale.components[0] = 10;
+box.scale.components[1] = 10;
+box.rotation.components[0] = 0;
+box.rotation.components[1] = 0;
 
-body2.setShape(shape2);
-body2.color="#00ff00";
-body2.scale=new Vector(10, 10);
+const ball = PhysicsObject.CIRCLE();
+ball.position.components[0] = 40;
+ball.position.components[1] = 40;
+ball.scale.components[0] = 40;
+ball.scale.components[1] = 40;
+
+
 const animation = () => {
     canvas.clear();
-    body.rotation.add(new Vector(0.1, 0.1));
-    // body.addForce(new Vector((Math.random()-0.5), (Math.random()-0.5)));
-    // body2.addForce(new Vector((Math.random()-0.5)*2, (Math.random()-0.5)*2));
-    body.update(canvas.ctx);
-    body2.update(canvas.ctx);
+    canvas.grid();
+    // box.update(canvas.ctx);
+    // ball.update(canvas.ctx);
     requestAnimationFrame(animation);
 }
 
