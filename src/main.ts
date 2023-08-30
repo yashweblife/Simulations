8c50c355b3abd6a23e4c2a57435b76684e623a99
yashweblife <yashweblife20@gmail.com>
2023-08-30 16:19:08 -0500

    add world

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,38 @@
-import { Canvas } from "./lib";
+import { Canvas, Vector } from "./lib";
 import { PhysicsObject } from "./lib/PhysicsObject/PhysicsObject";
 
 const canvas = new Canvas();
 
-const box = PhysicsObject.BOX();
-box.position.components[0] = 0;
-box.position.components[1] = 0;
-box.scale.components[0] = 20;
-box.scale.components[1] = 20;
-box.rotation.components[0] = 0;
-box.rotation.components[1] = 0;
-box.color = "blue";
+export class World{
+    public objects:PhysicsObject[] = [];
+    public inView:PhysicsObject[] = [];
+    public origing:Vector = new Vector(0,0);
+    public zoom = 1;
+    public rotation:Vector = new Vector(0,0);
+    public backgroundColor:string = "#000000";
+    public canvas:Canvas = new Canvas();
+    constructor(){
+        this.objects = [];
+        this.inView = [];
+        this.origing = new Vector(0,0);
+        this.zoom=1;
+        this.rotation = new Vector(0,0);
+    }
+    public add(object:PhysicsObject){
+        this.objects.push(object);
+    }
+    public update(ctx:CanvasRenderingContext2D){
+       
+        this.inView.forEach((object)=>{
+            object.update(ctx);
+        });
+    }
+}
 
-const ball = PhysicsObject.CIRCLE();
-ball.position.components[0] = 40;
-ball.position.components[1] = 40;
-ball.velocity.components[0] = 0;
-ball.velocity.components[1] = 0;
-ball.scale.components[0] = 40;
-ball.scale.components[1] = 40;
-ball.color = "red";
 
-console.log(ball)
 const animation = () => {
     canvas.clear();
     canvas.grid();
-    box.update(canvas.ctx);
-    ball.update(canvas.ctx);
     requestAnimationFrame(animation);
 }
 
