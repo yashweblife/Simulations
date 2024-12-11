43d7f390365c504a66bb926783c6eba10dd91143
yashweblife <yashweblife20@gmail.com>
2024-12-11 16:48:00 -0600

    important methods

--- src/lib/Canvas/index.ts
+++ src/lib/Canvas/index.ts
@@ -7,7 +7,7 @@ export default class Canvas{
         c.width=300;
         c.height=300;
     }
-    arc(x:number,y:number,radius:number, stroke:boolean, color:string="red"){
+    arc(x:number,y:number,radius:number, stroke:boolean=false, color:string="red"){
         this.c.beginPath();
         this.c.arc(x,y,radius,0,Math.PI*2);
         if(stroke){
@@ -18,5 +18,32 @@ export default class Canvas{
             this.c.fill();
         }
         this.c.closePath();
+    }
+    rect(x:number,y:number,w:number,h:number, stroke:boolean=false, color:string="red"){
+        this.c.beginPath();
+        this.c.rect(x,y,w,h);
+        if(stroke){
+            this.c.strokeStyle=color;
+            this.c.stroke();
+        }else{
+            this.c.fillStyle=color;
+            this.c.fill();
+        }
+        this.c.closePath();
+    }
+    clear(){
+        this.c.clearRect(0,0,this.width,this.height);
+    }
+    fill(color:string){
+        this.c.fillStyle=color;
+        this.c.fill();
+    }
+    stroke(color:string){
+        this.c.strokeStyle=color;
+        this.c.stroke();
+    }
+    fillRect(x:number,y:number,w:number,h:number, color:string="red"){
+        this.c.fillStyle=color;
+        this.c.fillRect(x,y,w,h);
     }
 }
