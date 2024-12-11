4ba4edaf7e83b0ef97ebc0972f2d1762d7a2005b
yashweblife <yashweblife20@gmail.com>
2024-12-11 16:44:51 -0600

    implement canvas object

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useRef, useState } from "react";
+import Canvas from "./lib/Canvas";
+
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [canvas, setCanvas] = useState<Canvas | null>(null);
+  
+  useEffect(()=>{
+    if(canvasRef.current){
+      setCanvas(new Canvas(canvasRef.current));
+    }
+  },[canvasRef])
   return (
     <div className="App">
+      <canvas ref={canvasRef} style={{boxShadow:"0px 0px 10px rgba(0,0,0,0.2)"}}></canvas>
     </div>
   )
 }
